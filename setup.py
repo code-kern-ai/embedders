948d056f3e8c946acfe730a83a67f0107b096040
felix0496 <felix.kirsch@kern.ai>
2022-10-20 16:09:18 +0200

    Slim down requirements

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(this_directory, "README.md")) as file:
 
 setup(
     name="embedders",
-    version="0.0.17",
+    version="0.0.18",
     author="Johannes Hötter",
     author_email="johannes.hoetter@kern.ai",
     description="High-level API for creating sentence and token embeddings",
@@ -25,15 +25,10 @@ setup(
     package_dir={"": "."},
     packages=find_packages("."),
     install_requires=[
-        "huggingface-hub",
-        "nltk",
         "numpy",
         "scikit-learn",
-        "scipy",
         "sentence-transformers",
-        "sentencepiece",
         "spacy>=3.0.0",
-        "tokenizers>=0.10.3",
         "torch>=1.6.0",
         "tqdm",
         "transformers>=4.6.0,<5.0.0",
